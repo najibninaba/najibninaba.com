4be79a12da4876689afe5eb762c1f862685611d2
Najib Ninaba <najibninaba@gmail.com>
2025-10-04 15:24:31 +0800

    Use custom domain najibninaba.com (CNAME) and update Astro site/base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://najibninaba.github.io',
-  base: '/najibninaba.com',
+  site: 'https://najibninaba.com',
+  base: '/',
 });
